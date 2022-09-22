1d0eee6cc9fbc06f2072a01eca008ae207c18da0
metacode22 <alohajune22@gmail.com>
2022-09-22 14:06:20 +0900

    feat: Logout 컴포넌트 구현, 로그인 후 화면 구성

---
+++ client/src/components/common/Logout.jsx
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { ACCESS_TOKEN } from "../../consts";
+import { removeStorageItem } from "../../lib/util";
+
+function Logout() {
+  const navigate = useNavigate();
+  
+  const handleButtonClick = () => {
+    removeStorageItem(ACCESS_TOKEN);
+    navigate('/');
+  }
+  
+  return <Wrap onClick={handleButtonClick}>로그아웃</Wrap>
+}
+
+export default Logout;
+
+const Wrap = styled.div`
+  border: 1px solid black;
+  height: 3em;
+  cursor: pointer;
+  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  :hover {
+    background-color: #e2e5e6;
+  }
+`
--- client/src/components/common/Sider.jsx
+++ client/src/components/common/Sider.jsx
@@ -1,7 +1,20 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import Logout from './Logout';
 
 function Sider() {
-  return <Wrap>sider</Wrap>
+  
+  const handleButtonClick = () => {
+    
+  }
+  
+  return (
+    <Wrap>
+      <div>Logo</div>
+      <ButtonWrap>계좌 목록</ButtonWrap>
+      <ButtonWrap>사용자</ButtonWrap>
+      <Logout />
+    </Wrap>
+  );
 }
 
 export default Sider;
@@ -9,5 +22,18 @@ export default Sider;
 const Wrap = styled.aside`
   width: 100%;
   border: 1px solid black;
+`;
+
+const ButtonWrap = styled.div`
+  border: 1px solid black;
+  height: 3em;
+  cursor: pointer;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
   
-`
+  :hover {
+    background-color: #e2e5e6;
+  }
+`;
